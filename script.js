b141eb81953b5638a0656d4b65448ebfbaf44e49
Lazar Narcis <lnarcis310@gmail.com>
2022-04-25 15:17:49 +0300

    save all numbers in a let type array

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let lotto6 = document.querySelector("#lotto6-49"); //lotto 6/49 div
 let elements = 49;
 let lotto_type = document.querySelector("#lotto_type");
 let maxElements = 0;
+let numbers = [];
 
 //set lotto type
 lotto_type.innerHTML = "Lotto 6/49";
@@ -15,13 +16,21 @@ for (let i = 1; i <= elements; i++) {
             if (maxElements < 6) {
                 element.style.background = "red";
                 maxElements++;
+                numbers.push(i);
+                console.log(numbers);
             } else {
                 alert('you can choose max 6 elements');
             }
         } else {
             element.style.background = "green";
             maxElements--;
+            numbers.splice(numbers.indexOf(i), 1);
+            console.log(numbers);
         }
     });
     lotto6.appendChild(element);
+}
+
+function bet () {
+    alert(numbers);
 }
