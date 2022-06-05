562d55d4cae12156cbf3c1ccbffd81cdc5130199
Lazar Narcis <lnarcis310@gmail.com>
2022-06-05 14:28:08 +0300

    add readme.md

---
+++ readme.md
@@ -0,0 +1,13 @@
+# lotto
+
+Lotto application. We select certain numbers and bet on them.
+
+## Programming Languages
+
+The programming languages used for this project are: <b>html</b>, <b>css</b> and <b>javascript</b>.
+
+Link to app: https://lazarnarcis.github.io/lotto/
+
+<hr>
+
+<p align="center">This project is made by <a href="https://lazarnarcis.github.io">Narcis</a></p>
--- script.js
+++ script.js
@@ -7,31 +7,39 @@ let numbers = localStorage.getItem("numbers") || [];
 //set lotto type
 lotto_type.innerHTML = "Lotto 6/49";
 
-for (let i = 1; i <= elements; i++) {
-    let element = document.createElement("div");
-    element.id = "element";
-    element.innerHTML = i;
-    element.addEventListener("click", () => {
-        if (element.style.background != "red") {
-            if (maxElements < 6) {
-                element.style.background = "red";
-                maxElements++;
-                numbers = [...numbers, i];
+function makeNumbers () {
+    for (let i = 1; i <= elements; i++) {
+        let element = document.createElement("div");
+        element.id = "element";
+        element.innerHTML = i;
+        let finalNumber = {
+            number: i,
+            selected: false
+        };
+        element.addEventListener("click", () => {
+            if (element.style.background != "red") {
+                if (maxElements < 6) {
+                    meelent.style.background = "red";
+                    maxElements++;
+                    numbers = [...numbers, finalNumbers];
+                    localStorage.setItem("numbers", numbers);
+                    console.log(numbers);
+                } else {
+                    alert('you can choose max 6 numbers');
+                }
+            } else {
+                element.style.background = "green";
+                maxElements--;
+                numbers.splice(numbers.indexOf(i), 1);
                 localStorage.setItem("numbers", numbers);
                 console.log(numbers);
-            } else {
-                alert('you can choose max 6 numbers');
             }
-        } else {
-            element.style.background = "green";
-            maxElements--;
-            numbers.splice(numbers.indexOf(i), 1);
-            localStorage.setItem("numbers", numbers);
-            console.log(numbers);
-        }
-    });
-    lotto6.appendChild(element);
+        });
+        lotto6.appendChild(element);
+    }
 }
+
+makeNumbers();
 
 function bet () {
     if (numbers.length != 6) {
